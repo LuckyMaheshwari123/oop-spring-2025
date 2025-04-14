b807c96c373fe42b51875e15b10b3340d3b68340
LA Maheshwari <lamaheshwari9@gmail.com>
2025-04-14 11:12:44 +0500

    Update task3.cpp

--- lab9/task3.cpp
+++ lab9/task3.cpp
@@ -1,18 +1,22 @@
-#include<bits/stdc++.h>
-#include"book.h";
-
+#include <iostream>
+#include "book.h"
 
 using namespace std;
 
+int main()
+{
+    Book *b1 = new Book("work Hard Enough to get Lucky", "Lucky Maheshwari", "123456789");
+    Book *b2 = new Book("Fiction by srk", "ShahRukh Khan", "12345678978");
+    Book *b3 = new Book("From Rags to Rich", "Elon Musk", "12345678689");
+    cout << "The title is:" << b1->getTitle() << endl;
+    cout << "The author is:" << b1->getAuthor() << endl;
+    cout << "The isbn is:" << b1->getIsbn() << endl;
+    Library l1;
+    l1.addBook(*b1);
+    l1.addBook(*b2);
+    l1.addBook(*b3);
+    l1.searchBook("123456789");
+    l1.removeBook("12345678922");
 
-
-int main(){
- Book b1("work Hard Enough to get Lucky","Lucky Maheshwari","123456789");
- cout<<"The title is:"<<b1.getTitle()<<endl;
- cout<<"The author is:"<<b1.getAuthor()<<endl;
- cout<<"The isbn is:"<<b1.getIsbn()<<endl;
-
-
-
-return 0;
+    return 0;
 }
