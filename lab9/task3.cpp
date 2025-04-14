47e9dc9340e104a0a793ee484577823c557be240
LA Maheshwari <lamaheshwari9@gmail.com>
2025-04-14 10:37:14 +0500

    Create task3.cpp

---
+++ lab9/task3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include"book.h";
+
+
+using namespace std;
+
+
+
+int main(){
+ Book b1("work Hard Enough to get Lucky","Lucky Maheshwari","123456789");
+ cout<<"The title is:"<<b1.getTitle()<<endl;
+ cout<<"The author is:"<<b1.getAuthor()<<endl;
+ cout<<"The isbn is:"<<b1.getIsbn()<<endl;
+
+
+
+return 0;
+}
